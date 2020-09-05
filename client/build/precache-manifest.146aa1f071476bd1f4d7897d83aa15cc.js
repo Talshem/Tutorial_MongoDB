2505b29178180cc2f0b907c82dcc6a669e800c33
Talshem <talsh8797@gmail.com>
2020-09-05 16:34:49 +0300

    build

---
+++ client/build/precache-manifest.146aa1f071476bd1f4d7897d83aa15cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc2c134774dd1db5a8aa078f874fe02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e353b52844322c2b5732",
+    "url": "/static/css/main.3eb53174.chunk.css"
+  },
+  {
+    "revision": "c5f4778889abe6d8a014",
+    "url": "/static/js/2.dcf70ba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dcf70ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e353b52844322c2b5732",
+    "url": "/static/js/main.e9dc1f73.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
